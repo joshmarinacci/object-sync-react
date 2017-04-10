15ccabeb4b38b48d1f2e4429e5c361c185317116
josh <joshua@marinacci.org>
2017-04-10 11:05:08 -0700

    initial import

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          foo
+      </div>
+    );
+  }
+}
+
+export default App;
